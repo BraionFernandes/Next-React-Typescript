896b63406dd4d9f2b805227240c2e94c966694ae
BraionFernandes <132958791+BraionFernandes@users.noreply.github.com>
2026-05-06 15:53:41 -0300

    Next-React - Estudos - CFBCursos

---
+++ Estudos/CFBCursos/aula3/src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+const nome="Bruno";
+let canal="CFBCursos";
+
+export default function Home() {
+  return (
+    <div>
+      <div>{nome}</div>
+      <div>CFBCursos</div>
+      <div>Curso de react Next</div>
+      <div>Typescript</div>
+      <div>React</div>
+    </div>
+  );
+}
